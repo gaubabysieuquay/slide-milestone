52c19eea8a0f1ab081209583a7843a2ec9e39b3c
gaubabysieuquay <dungnguyenduong27@gmail.com>
2022-11-02 04:13:00 +0700

    Implement logarithm slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,23 +15,22 @@ import { Slider } from 'primeng/slider';
   styleUrls: ['app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('slider', { static: false }) slider: Slider;
-  sliderEl: ElementRef;
+export class AppComponent {
   max: number = 2500;
-  min: number = 0;
+  min: number = 1;
   form: FormGroup;
   displayValue: string | number;
+  minValue: number = 0;
+  maxValue: number = 2500;
+  value: number = 0;
+  scale: any = 0;
+  position = 0;
 
-  constructor(private fb: FormBuilder, private renderer: Renderer2) {
+  constructor(private fb: FormBuilder) {
     this.initForm();
   }
 
   ngOnInit() {}
-
-  ngAfterViewInit() {
-    this.sliderEl = this.slider.el.nativeElement;
-  }
 
   initForm = () => {
     this.form = this.fb.group({
@@ -40,18 +39,84 @@ export class AppComponent implements AfterViewInit {
   };
 
   stepIncrement = () => {
-    const step = this.changeValueDisplay();
+    let data = this.getValues(true);
+    let val = 0;
+    let calStep = this.roundNumber(this.getValueFromPosition(data.value, data));
+
     switch (true) {
-      case step >= 0 && step < 10:
-        return 0.1;
-      case step >= 10 && step < 100:
-        return 1;
+      case calStep >= 0 && calStep < 10:
+        val = calStep + 0.1;
+        break;
+      case calStep >= 10 && calStep <= 100:
+        calStep = this.roundNumber(
+          this.getValueFromPosition(data.value, data),
+          0
+        );
+        val = calStep + 1;
+        break;
       default:
-        return 100;
+        val = calStep + 100;
+        break;
     }
+
+    const prevStep = this.roundNumber(
+      this.getPositionFromValue(data, calStep),
+      6
+    );
+    const nextStep = this.roundNumber(this.getPositionFromValue(data, val), 6);
+
+    return nextStep - prevStep;
   };
 
-  changeValueDisplay = () => {
-    return this.form.get('distance')!.value;
+  changeValue = (event: any) => {
+    let data = this.getValues();
+    this.setPositionValue(event.value, data);
+  };
+
+  setPositionValue(position: any, data: any) {
+    this.value = this.roundNumber(this.getValueFromPosition(position, data));
+    this.form.controls['distance'].setValue(position);
+  }
+
+  getValues(step = false) {
+    if (!step) {
+      this.value = this.form.controls['distance'].value
+        ? this.form.controls['distance'].value
+        : this.value;
+    }
+
+    let data = {
+      min: Math.log(this.min),
+      max: Math.log(this.max),
+      scale: +this.scale,
+      value: step ? this.form.controls['distance'].value : +this.value,
+      position: this.position,
+      minValue: this.minValue,
+      maxValue: this.maxValue,
+    };
+
+    data['scale'] = (data.max - data.min) / (data.maxValue - data.minValue);
+    if (data.value) {
+      data['position'] = this.getPositionFromValue(data);
+    }
+    return data;
+  }
+
+  // Calculate slider value from a position
+  getValueFromPosition(position: any, data: any) {
+    return Math.exp((position - data.minValue) * data.scale + data.min);
+  }
+
+  // Calculate slider position from a value
+  getPositionFromValue(data: any, step: number = 0) {
+    return (
+      data.minValue +
+      (Math.log(step ? step : data.value) - data.min) / data.scale
+    );
+  }
+
+  roundNumber = (num: number, decimal = 1) => {
+    let decimalNum = Math.pow(10, decimal);
+    return Math.round((num + Number.EPSILON) * decimalNum) / decimalNum;
   };
 }
--- src/app/shared/slider-milestone-directive/slider-milestone.directive.ts
+++ src/app/shared/slider-milestone-directive/slider-milestone.directive.ts
@@ -11,12 +11,17 @@ import { NgControl } from '@angular/forms';
   selector: '[appSliderMilestone]',
 })
 export class SliderMilestoneDirective implements AfterViewInit {
-  @Input() milestone: number[] = [10, 100, 1000];
+  @Input() milestone: number[] = [10, 100];
   @Input() min: number = 0;
   @Input() max: number;
   @Input() equalizeSpacing: boolean = false;
+  @Input() minValue: any = 1;
+  @Input() maxValue: any = 1000;
+  value: number;
   nodes = [];
   exactWidth: number = 0;
+  scale = 0;
+  position = 0;
 
   constructor(
     private elRef: ElementRef,
@@ -26,14 +31,10 @@ export class SliderMilestoneDirective implements AfterViewInit {
 
   ngAfterViewInit() {
     this.addMilestone();
-    console.log(this.elRef.nativeElement.querySelector('.p-slider'));
   }
 
   ngOnInit() {
     this.sortAndCheckDuplicate();
-    // this.control.control?.valueChanges.subscribe((value: number) =>
-    //   console.log(value)
-    // );
   }
 
   addMilestone = () => {
@@ -76,14 +77,44 @@ export class SliderMilestoneDirective implements AfterViewInit {
     });
   };
 
-  equalAllMilestoneSectionWidth = () => {
-    const countMilestone = this.milestone.length;
-    const calWidth = this.max / countMilestone;
-  };
-
   sortAndCheckDuplicate = () => {
     this.milestone.push(this.max, this.min);
     this.milestone.sort((a, b) => a - b);
     this.milestone = [...new Set(this.milestone)];
   };
+
+  setPositionValue(position: any, data: any) {
+    this.value = +this.getValueFromPosition(position, data).toFixed(1);
+    this.control.control!.setValue(position);
+  }
+
+  getValues() {
+    this.value = this.control.control!.value;
+
+    let data = {
+      min: Math.log(this.min),
+      max: Math.log(this.max),
+      scale: +this.scale,
+      value: +this.value,
+      position: this.position,
+      minValue: this.minValue,
+      maxValue: this.maxValue,
+    };
+    console.log(data);
+    data['scale'] = (data.max - data.min) / (data.maxValue - data.minValue);
+    if (data.value) {
+      data['position'] = this.getPositionFromValue(data);
+    }
+    return data;
+  }
+
+  // Calculate slider value from a position
+  getValueFromPosition(position: any, data: any) {
+    return Math.exp((position - data.minValue) * data.scale + data.min);
+  }
+
+  // Calculate slider position from a value
+  getPositionFromValue(data: any) {
+    return data.minValue + (Math.log(data.value) - data.min) / data.scale;
+  }
 }
