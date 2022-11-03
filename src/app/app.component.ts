99e1708552fd19045217ad52baa07a62be478a7c
gaubabysieuquay <dungnguyenduong27@gmail.com>
2022-11-04 03:24:58 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   maxValue = 2500;
   value = 0;
   scale = 0;
-  position = 0;
+  position = 1;
   displayValue: string | number;
 
   constructor(private fb: FormBuilder) {
@@ -26,38 +26,45 @@ export class AppComponent {
 
   initForm = () => {
     this.form = this.fb.group({
-      distance: [0, Validators.min(0)],
+      distance: [1, Validators.min(1)],
     });
   };
 
   stepIncrement = () => {
-    const data = this.getValues(true);
-    let val = 0;
-    let calStep = this.roundNumber(this.getValueFromPosition(data.value, data));
+    const data = this.getValues();
+    let currPos = 0;
+
+    let nextPos = 0;
 
     switch (true) {
-      case calStep >= 0 && calStep < 10:
-        val = calStep + 0.1;
-        break;
-      case calStep >= 10 && calStep <= 100:
-        calStep = this.roundNumber(
-          this.getValueFromPosition(data.value, data),
+      case data.value < 10:
+        return 1;
+      case data.value < 100:
+        currPos = this.roundNumber(data.position, 'ceil', 0);
+        nextPos = this.roundNumber(
+          this.getPositionFromValue(data, data.value + 1),
+          'ceil',
           0
         );
-        val = calStep + 1;
         break;
       default:
-        val = calStep + 100;
+        currPos = this.roundNumber(data.position, 'ceil', 0);
+        nextPos = this.roundNumber(
+          this.getPositionFromValue(data, data.value + 100),
+          'ceil',
+          0
+        );
         break;
     }
-
-    const prevStep = this.roundNumber(
-      this.getPositionFromValue(data, calStep),
-      6
+    console.log(
+      '🚀 ~ file: app.component.ts ~ line 36 ~ AppComponent ~ currPos',
+      currPos
     );
-    const nextStep = this.roundNumber(this.getPositionFromValue(data, val), 6);
-
-    return nextStep - prevStep;
+    console.log(
+      '🚀 ~ file: app.component.ts ~ line 38 ~ AppComponent ~ nextPos',
+      nextPos
+    );
+    return nextPos - currPos;
   };
 
   changeValue = (event: any) => {
@@ -66,30 +73,42 @@ export class AppComponent {
   };
 
   setPositionValue(position: any, data: any) {
-    this.value = this.roundNumber(this.getValueFromPosition(position, data));
-    this.form.controls['distance'].setValue(position);
+    if (this.value < 10) {
+      this.value = this.roundNumber(
+        this.getValueFromPosition(position, data),
+        'floor'
+      );
+    } else {
+      this.value = this.roundNumber(
+        this.getValueFromPosition(position, data),
+        'floor',
+        0
+      );
+    }
+    // this.form.controls['distance'].setValue(position);
   }
 
-  getValues(step = false) {
-    if (!step) {
-      this.value = this.form.controls['distance'].value
-        ? this.form.controls['distance'].value
-        : this.value;
-    }
-
+  getValues() {
     const data = {
       min: Math.log(this.min),
       max: Math.log(this.max),
       scale: +this.scale,
-      value: step ? this.form.controls['distance'].value : +this.value,
+      value: +this.value,
       position: this.position,
       minValue: this.minValue,
       maxValue: this.maxValue,
     };
+    console.log(
+      '🚀 ~ file: app.component.ts ~ line 73 ~ AppComponent ~ getValues ~ data',
+      data
+    );
 
     data['scale'] = (data.max - data.min) / (data.maxValue - data.minValue);
     if (data.value) {
-      data['position'] = this.getPositionFromValue(data);
+      data['position'] =
+        data.value > 10
+          ? this.roundNumber(this.getPositionFromValue(data), 'ceil', 0)
+          : this.getPositionFromValue(data);
     }
     return data;
   }
@@ -107,8 +126,16 @@ export class AppComponent {
     );
   }
 
-  roundNumber = (num: number, decimal = 1) => {
+  roundNumber = (num: number, roundType = 'round', decimal = 1) => {
     const decimalNum = Math.pow(10, decimal);
-    return Math.round((num + Number.EPSILON) * decimalNum) / decimalNum;
+
+    switch (roundType) {
+      case 'ceil':
+        return Math.ceil(num * decimalNum) / decimalNum;
+      case 'floor':
+        return Math.floor(num * decimalNum) / decimalNum;
+      default:
+        return Math.round((num + Number.EPSILON) * decimalNum) / decimalNum;
+    }
   };
 }
